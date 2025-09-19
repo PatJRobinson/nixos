36f4c42e62489aee8626ab9b3a09c9a6bfeb2d21
PatJRobinson <robinsonpaddy4@gmail.com>
2025-09-19 23:45:39 +0100

    fix p10k config

--- users/paddy/home.nix
+++ users/paddy/home.nix
@@ -1,12 +1,21 @@
 { config, pkgs, ... }:
 
 let 
+
+    hyprRepo = pkgs.fetchFromGitHub {
+      owner = "PatJRobinson";
+      repo = "hyprland-dotfiles";
+      rev = "/refs/tags/v1.0.0-laptop";
+      sha256 = "1fx2l2h4bd6754d3lp5nhh5crhgbkhfvk5ffc3lg44cv75wpis3g";
+    };
+
     waybarRepo = pkgs.fetchFromGitHub {
       owner = "PatJRobinson";
       repo = "WaybarTheme";
       rev = "/refs/tags/v1.0.0";
       sha256 = "1xy3zbvxk3qqa2h1l2qwfhwhmvm4ri2d3b7bhr0qi4wxim299mz7";
     };
+
 in
 {
   # Basic info
@@ -24,14 +33,20 @@ in
       enable = true;
       plugins = [
         { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+        {
+          name = "romkatv/powerlevel10k"; 
+          tags = [ as:theme depth:1 ]; 
+        } # Installations with additional options. For the list of options, please refer to Zplug README.
       ];
     };
 
     oh-my-zsh = { # "ohMyZsh" without Home Manager
       enable = true;
-      theme = "powerlevel10k/powerlevel10k";
     };
+
+    initExtra = ''
+      [[ ! -f ${./p10k-config/.p10k.zsh} ]] || source ${./p10k-config/.p10k.zsh}
+    '';
  };
 
   programs.git.enable = true;
@@ -60,5 +75,6 @@ in
   ];
 
   home.file.".config/waybar".source = waybarRepo;
+  home.file.".config/hypr".source = hyprRepo;
 }
 
