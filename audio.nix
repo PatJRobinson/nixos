91fa32794f4817abe2768a0d48cbcd600c22b9c4
PatJRobinson <robinsonpaddy4@gmail.com>
2025-09-19 11:46:14 +0100

    adds audio config

--- audio.nix
+++ audio.nix
@@ -4,4 +4,27 @@
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
+
+  security.rtkit.enable = true; # Enable RealtimeKit for audio purposes
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # Uncomment the following line if you want to use JACK applications
+    # jack.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+  ];
+
+  #
+  # Bluetooth
+  #
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
 }
