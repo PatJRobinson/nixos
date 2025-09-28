10c66c51ea320bdbfa2f238cd5d16856b2fe35f6
PatJRobinson <robinsonpaddy4@gmail.com>
2025-09-28 15:45:47 +0100

    remove useless module

--- hosts/pj-laptop/configuration.nix
+++ hosts/pj-laptop/configuration.nix
@@ -34,7 +34,6 @@
     ../../modules/firejail.nix
     ../../modules/filesystem.nix
     ../../modules/power.nix
-    ../../modules/utils.nix
     ../../modules/backlight.nix
   ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
--- modules/users.nix
+++ modules/users.nix
@@ -7,9 +7,14 @@
   users.users.paddy = {
     isNormalUser = true;
     description = "Paddy";
-    extraGroups = [ "networkmanager" "wheel" "docker"];
+    extraGroups = [ "networkmanager" "wheel" "docker" "video"];
     packages = with pkgs; [
     #  thunderbird
     ];
   };
+
+  # udev rule to set the backlight node group and mode
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chgrp video %S%p/brightness", RUN+="${pkgs.coreutils}/bin/chmod g+w %S%p/brightness"
+  '';
 }
