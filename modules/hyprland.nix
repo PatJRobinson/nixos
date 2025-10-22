272f8ebe045f001543b91825e107b1b21e0a5f47
PatJRobinson <robinsonpaddy4@gmail.com>
2025-10-22 17:22:57 +0100

    adds obsidian wayland override; adds usr to trusted

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,6 +1,12 @@
 { pkgs, ...}:
 
 {
+  environment.variables = {
+    "QT_QPA_PLATFORM" = "wayland";
+    "QT_USE_NATIVE_BORDER" = "1";
+    "NIXOS_OZONE_WL"="1";
+  };
+
   # hyprland config
   programs.hyprland.enable = true; # enable Hyprland
 
--- modules/text-editing.nix
+++ modules/text-editing.nix
@@ -1,8 +1,18 @@
 { pkgs, ...}:
 
+let
+
+  obsidianWayland = pkgs.writeShellScriptBin "obsidian-wayland" ''
+    exec ${pkgs.obsidian}/bin/obsidian \
+      --enable-features=UseOzonePlatform \
+      --ozone-platform=wayland \
+      "$@"
+  '';
+
+in
 {
   environment.systemPackages = with pkgs; [
     neovim
-    obsidian
+    obsidianWayland
   ];
 }
