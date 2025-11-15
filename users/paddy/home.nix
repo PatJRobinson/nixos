4ecdea29569b1245ffd69f9fcdad6e26a2930a60
PatJRobinson <robinsonpaddy4@gmail.com>
2025-11-15 20:38:58 +0000

    update nvim config (and adds lsp nix packages) to support nil_ls and lua_ls

--- users/paddy/home.nix
+++ users/paddy/home.nix
@@ -1,16 +1,24 @@
-{ config, pkgs, nixpkgsUrl, channel, userName, hyprParams, ... }:
+{
+  config,
+  pkgs,
+  nixpkgsUrl,
+  channel,
+  userName,
+  hyprParams,
+  ...
+}:
 
-let 
+let
 
-    neovimRepo = pkgs.fetchFromGitHub {
-      owner = "PatJRobinson";
-      repo = "kickstart.nvim";
-      rev = "/refs/heads/master";
-      sha256 = "sha256-3PnyAYxOUapuPfDd+AHL8JHPPQcKUhL/zJEsrNkW2fQ=";
-    };
+  neovimRepo = pkgs.fetchFromGitHub {
+    owner = "PatJRobinson";
+    repo = "kickstart.nvim";
+    rev = "/refs/heads/master";
+    sha256 = "sha256-PkmC9iC5CkBmO+fduGuB/wNPT2VcAxkOq5yopFmKlSc=";
+  };
 
-    wallpapers_dir = 
-      if hyprParams.displayType == "ultrawide" then ./wallpapers-ultrawide else ./wallpapers;
+  wallpapers_dir =
+    if hyprParams.displayType == "ultrawide" then ./wallpapers-ultrawide else ./wallpapers;
 in
 {
   # Basic info
@@ -29,36 +37,40 @@ in
       plugins = [
         { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
         {
-          name = "romkatv/powerlevel10k"; 
-          tags = [ as:theme depth:1 ]; 
+          name = "romkatv/powerlevel10k";
+          tags = [
+            "as:theme"
+            "depth:1"
+          ];
         } # Installations with additional options. For the list of options, please refer to Zplug README.
       ];
     };
 
-    oh-my-zsh = { # "ohMyZsh" without Home Manager
+    oh-my-zsh = {
+      # "ohMyZsh" without Home Manager
       enable = true;
     };
 
     initContent = ''
-      [[ ! -f ${./p10k-config/.p10k.zsh} ]] || source ${./p10k-config/.p10k.zsh}
+        [[ ! -f ${./p10k-config/.p10k.zsh} ]] || source ${./p10k-config/.p10k.zsh}
 
-      pw() {
-        if [[ $# -lt 1 ]]; then
-          echo "Usage: bwcopy <item-name-or-id>"
-          return 1
+        pw() {
+          if [[ $# -lt 1 ]]; then
+            echo "Usage: bwcopy <item-name-or-id>"
+            return 1
+          fi
+
+          bw get password "$@" | wl-copy
+       }
+
+      function f() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+        yazi "$@" --cwd-file="$tmp"
+        if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+          builtin cd -- "$cwd"
         fi
-
-        bw get password "$@" | wl-copy
-     }
-
-    function f() {
-      local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-      yazi "$@" --cwd-file="$tmp"
-      if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-        builtin cd -- "$cwd"
-      fi
-      rm -f -- "$tmp"
-    }
+        rm -f -- "$tmp"
+      }
 
     '';
   };
@@ -73,7 +85,7 @@ in
   # Example environment variables
   home.sessionVariables = {
     EDITOR = "nvim";
-    PAGER  = "less";
+    PAGER = "less";
   };
 
   # Packages to install
@@ -87,11 +99,13 @@ in
     bitwarden-cli
     libnotify
     nix-direnv
+    lua-language-server
+    nil
   ];
 
   imports = [
-    (import ./modules/hyprland.nix { inherit hyprParams; } )
-    (import ./modules/visualisation.nix )
+    (import ./modules/hyprland.nix { inherit hyprParams; })
+    (import ./modules/visualisation.nix)
   ];
 
   programs.ssh = {
@@ -117,15 +131,14 @@ in
   programs.kitty.enable = true;
   home.file.".config/kitty".source = ./kitty;
   # set gruvbox theme
-  home.file.".config/ghostty/config".text = 
-    if channel == "25.05" then 
-      "theme = GruvboxDarkHard" else "theme = Gruvbox Dark Hard";
+  home.file.".config/ghostty/config".text =
+    if channel == "25.05" then "theme = GruvboxDarkHard" else "theme = Gruvbox Dark Hard";
 
   programs.waybar.enable = true;
   # src: https://github.com/poetaste/dotfiles.git
   home.file.".config/waybar".source = ./waybar;
 
   home.file.".config/nvim".source = neovimRepo;
-  home.file.".local/firejail/qute-casual/.config/qutebrowser/config.py".source = ./qutebrowser/config.py;
+  home.file.".local/firejail/qute-casual/.config/qutebrowser/config.py".source =
+    ./qutebrowser/config.py;
 }
-
