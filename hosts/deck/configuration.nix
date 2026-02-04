81ab758318f69ea2f03678fa196897a6fec9158e
PatJRobinson <robinsonpaddy4@gmail.com>
2026-02-04 20:08:16 +0000

    update lock + jovian hash

--- hosts/deck/configuration.nix
+++ hosts/deck/configuration.nix
@@ -30,7 +30,7 @@
         builtins.fetchTarball {
           url = "https://github.com/Jovian-Experiments/Jovian-NixOS/archive/${revision}.tar.gz";
           # Update the hash as needed:
-          sha256 = "sha256:0wl2hz8b8wryixd08lf66gl8ijxqaz2682f8j1l75jmz3258nb7s";
+          sha256 = "sha256:1cgg8mq7246d3yrwvqqjz4frhq933hpjk5fz4lzj04s352qivj6y";
         }
         + "/modules"
     )
