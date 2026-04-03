2587c67435481146d8f2e718e31103aa5233cf1f
paddy@pj-desktop <patrick.james.robinson@proton.me>
2026-04-03 14:59:05 +0100

    adds likely fix to audio-hiss-on-boot bug with nvidia

--- modules/nvidia-graphics.nix
+++ modules/nvidia-graphics.nix
@@ -5,4 +5,9 @@
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia.open = true; # or false if you're intentionally using proprietary
   hardware.nvidia.modesetting.enable = true;
+
+  # isable HDA power saving at the kernel driver level - help with audio hiss on boot
+  boot.extraModprobeConfig = ''
+    options snd_hda_intel power_save=0 power_save_controller=N
+  '';
 }
